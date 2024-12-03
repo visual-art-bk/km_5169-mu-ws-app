943ebb4e13dd3067b31413f4f04c7e5e6e772fb7
visual-art-bk <kekekj@naver.com>
2024-12-03 15:48:53 +0900

    엑셀저장을 ui로 이전

--- src/app/core/services/CrawlerThread.py
+++ src/app/core/services/CrawlerThread.py
@@ -12,11 +12,6 @@ from app.core.services.SeniumDravierManager import SeniumDravierManager
 from app.core.services.MusinsaScrapper import MusinsaScrapper
 from app.core.utils import FileMaker
 
-START_TIME = datetime.datetime(2024, 11, 30, 00, 30)  # 샘플 사용 시작 시간
-LIMIT_TIME = datetime.timedelta(minutes=3600)  # 사용 가능한 제한 시간 설정
-MAX_TITLES = 5  # 한 번에 수집할 블로그 타이틀의 최대 개수 설정
-
-
 class CrawlerThread(QtCore.QThread):
 
     # PyQt Signal 정의: UI 업데이트를 위해 필요한 정보를 전송
@@ -39,7 +34,7 @@ class CrawlerThread(QtCore.QThread):
 
     def run(self):
         try:
-            with SeniumDravierManager(headless=False) as manager:
+            with SeniumDravierManager(headless=True) as manager:
                 driver = manager.driver
 
                 self.scraper = MusinsaScrapper(driver=driver)
@@ -50,36 +45,38 @@ class CrawlerThread(QtCore.QThread):
                     by=By.XPATH,
                     expression='//button[@data-button-name="더보기"]',
                     sleep_for_loading=1,
-                    max_scroll_attempts=1,
+                    max_scroll_attempts=10,
                     timeout=10,
                 )
 
                 event_links = self.scraper.scrap_all_musinsa_event_link(
-                    max_scraping_size=2
+                    max_scraping_size=100
                 )
 
                 brands_info_list = self.scraper.open_link_and_scrap(
                     brand_links=event_links
                 )
 
-                FileMaker.save_to_excel_for_musinsa(
-                    column_order=[
-                        "브랜드",
-                        "영문명",
-                        "브랜드 페이지",
-                        # "키프리스 바로가기",
-                        "상호 / 대표자",
-                        "연락처",
-                        "E-mail",
-                        "사업자번호",
-                        "통신판매업신고",
-                        "영업소재지",
-                    ],
-                    file_name="test_file_maker_excel",
-                    infos_list=brands_info_list,
-                )
+                # FileMaker.save_to_excel_for_musinsa(
+                #     column_order=[
+                #         "브랜드",
+                #         "영문명",
+                #         "브랜드 페이지",
+                #         # "키프리스 바로가기",
+                #         "상호 / 대표자",
+                #         "연락처",
+                #         "E-mail",
+                #         "사업자번호",
+                #         "통신판매업신고",
+                #         "영업소재지",
+                #     ],
+                #     file_name="test_file_maker_excel",
+                #     infos_list=brands_info_list,
+                # )
 
-                print(brands_info_list)
+                # print(brands_info_list)
+
+                self.results = brands_info_list
 
         except WebDriverException as e:
             # WebDriver 관련 오류 처리
